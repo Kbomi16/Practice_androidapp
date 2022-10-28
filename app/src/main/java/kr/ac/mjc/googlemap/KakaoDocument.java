1a7e4e08404e26f944bb215df850cb572b7c7173
김보미 <kimbomi2172@naver.com>
2022-10-28 14:34:23 +0900

    구글맵 지도 검색 후 그 위치로 가기

---
+++ app/src/main/java/kr/ac/mjc/googlemap/KakaoDocument.java
@@ -0,0 +1,15 @@
+package kr.ac.mjc.googlemap;
+
+import java.util.ArrayList;
+
+public class KakaoDocument {
+    private ArrayList<KakaoLocation> documents;
+
+    public ArrayList<KakaoLocation> getDocuments() {
+        return documents;
+    }
+
+    public void setDocuments(ArrayList<KakaoLocation> documents) {
+        this.documents = documents;
+    }
+}
